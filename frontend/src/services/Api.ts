df1d8348ec086c9c590438f5546f8fb65bf52bba
HigorSkw <higor.skowronski@gmail.com>
2023-02-10 14:37:09 -0300

    fix: deploy

--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const prod = "";
+const prod = "https://api-contact-book.onrender.com";
 
 const dev = "http://localhost:3001";
 
 export const api = axios.create({
-  baseURL: dev,
-  timeout: 7000,
+  baseURL: prod,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
